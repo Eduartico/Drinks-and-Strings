4897dc2b71eff032b1a7e8b5ef7d7cf0faa1a470
Eduartico <duarte.eduardoeds@gmail.com>
2021-12-20 16:51:22 +0000

    Transformer Group Stuff Done

--- src/main/java/StringTransformerGroup.java
+++ src/main/java/StringTransformerGroup.java
@@ -1,8 +1,18 @@
 import java.util.List;
 
-public class StringTransformerGroup {
-    StringTransformerGroup(List<StringTransformer> trans){
-
+public class StringTransformerGroup implements StringTransformer
+{
+    List<StringTransformer> trans;
+    public StringTransformerGroup(List<StringTransformer> listGiven)
+    {
+        trans = listGiven;
     }
-
+    @Override
+    public void execute(StringDrink drink)
+    {
+        for(StringTransformer listSearched:trans)
+        {
+            listSearched.execute(drink);
+        }
+    }
 }
